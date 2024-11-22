2f63b2c6caeb35be16970e72f1404d9b02e7e5f0
Lê Đức Anh Phương <leducanhphuongdev@gmail.com>
2024-11-22 19:54:20 +0700

    them cross

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*', // Cho phép tất cả các domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(8000);
