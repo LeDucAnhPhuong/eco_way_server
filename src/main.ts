13d8684f1aa42a25c1b35b0de8a0752c00d3e99f
Lê Đức Anh Phương <leducanhphuongdev@gmail.com>
2024-11-24 04:02:13 +0700

    han che cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // Cho phép tất cả các domain
+    origin: 'https://eco-way.vercel.app', // Chỉ cho phép domain này
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
